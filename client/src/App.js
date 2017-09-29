b96c555e67a1beb0eacba19ce553bbafee662272
Michael Davis <mhdavis1993@gmail.com>
2017-09-29 19:58:04 -0400

    pages: Formatted page components to render
    - App.js: Added page components to app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import About from './pages/About/About';
+import Technologies from './pages/Technologies/Technologies';
+import Portfolio from './pages/Portfolio/Portfolio';
+import Contact from './pages/Contact/Contact';
 
 class App extends Component {
   render() {
     return (
-      <h1>Placeholder</h1>
+      <div>
+        <h1>Placeholder</h1>
+        <About />
+        <Technologies />
+        <Portfolio />
+        <Contact />
+      </div>
     );
   }
 }
---
+++ client/src/pages/Technologies/Technologies.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Technologies extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Technologies Section</h1>
+      </div>
+    );
+  }
+}
+
+export default Technologies;
