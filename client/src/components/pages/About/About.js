de8ac0e3bb34b511a8ce75d0c1842dea0d5868b1
Michael Davis <mhdavis1993@gmail.com>
2017-10-04 22:35:14 -0400

    About.js: Wrote out basic structure and styled
    - About.js: added addition divs and classnames for styling and spacing
    - About.css: created file and added styling and spacing elements using
      flexbox

--- client/src/components/pages/About/About.js
+++ client/src/components/pages/About/About.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import "./About.css";
 
 class About extends Component {
   render() {
     return (
-      <div>
-        <h1>About Section</h1>
+      <div className="about-container">
+        <div className="about-img"></div>
 
-        <Link to="/">
-          <button>Home</button>
-        </Link>
+        <div className="about-panel">
+
+          <div className="about-information">
+            <h1>About</h1>
+            <hr/>
+            <p>
+              Est qui natus et magnam dicta. Animi nesciunt odio omnis quam magnam laudantium. Temporibus sapiente in doloribus. Est libero rerum est culpa doloremque molestias vero. Perferendis nam repellendus et. Est qui numquam atque qui aperiam perferendis libero.
+              Est qui natus et magnam dicta.
+              <br/>
+              <br/>
+              Animi nesciunt odio omnis quam magnam laudantium. Temporibus sapiente in doloribus. Est libero rerum est culpa doloremque molestias vero. Perferendis nam repellendus et. Est qui numquam atque qui aperiam perferendis libero.
+            </p>
+          </div> {/*end about information*/}
+
+          <div className="button-div">
+            <Link to="/">
+              <button className="home-button">HOME</button>
+            </Link>
+          </div>
+
+        </div> {/* end about panel */}
       </div>
     );
   }
