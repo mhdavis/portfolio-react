ac91a9d133d6a7170bb4eb6c1fecbf0a5bbe2686
Michael Davis <mhdavis1993@gmail.com>
2017-10-08 18:41:09 -0400

    Wrote up Tech section
    - Tech.js/css: Built out and styled portfolio component
    - blarg.js: removed unnecessary file
    - TechItem: created component and styled

---
+++ client/src/components/TechItem/TechItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./TechItem.css";
+
+const TechItem = (props) => {
+  return (
+    <div className="tech-item"></div>
+  );
+};
+
+export default TechItem;
--- client/src/components/pages/Tech/Tech.js
+++ client/src/components/pages/Tech/Tech.js
@@ -1,15 +1,63 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import './Tech.css';
+import { Row, Col } from 'react-flexbox-grid';
+import TechItem from "./../../TechItem/TechItem";
 
 class Tech extends Component {
   render() {
     return (
-      <div>
-        <h1>Technologies Section</h1>
+      <div className="tech-container">
+        <Row>
+          <Col xs={12}>
+            <Row center="xs">
 
-        <Link to="/">
-          <button>Home</button>
-        </Link>
+              <Col xs={12} sm={12} md={10} lg={10}>
+                 {/* Tech Section Header */}
+                 <div className="tech-section-header">
+                  <h1 className="tech-primary-title">Technologies</h1>
+                 </div>
+                 {/* Tech Section Header */}
+
+                 {/* Tech Section Body */}
+                 <Row className="tech-section-body">
+                   <Col xs={12} className="tech-panel">
+                     <Row center="xs">
+                       <Col xs={12}>
+
+                         <h2 className="tech-line">I have worked with...</h2>
+
+                         <div className="tech-items-box">
+                           <TechItem />
+                           <TechItem />
+                           <TechItem />
+                           <TechItem />
+                           <TechItem />
+                           <TechItem />
+                           <TechItem />
+                           <TechItem />
+                           <TechItem />
+                           <TechItem />
+                           <TechItem />
+                           <TechItem />
+                         </div>
+
+                         <div className="tech-section-footer">
+                           <Link to="/">
+                             <button className="tech-home-button">HOME</button>
+                           </Link>
+                         </div>
+
+                       </Col>
+                     </Row>
+                   </Col>
+                 </Row>
+                 {/* Tech Section Body */}
+               </Col>
+
+            </Row>
+          </Col>
+        </Row>
       </div>
     );
   }
