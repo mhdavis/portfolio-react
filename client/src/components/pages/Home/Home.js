06893cbd4e81bb5789ba415a0712455bd7198071
Michael Davis <mhdavis1993@gmail.com>
2019-01-14 12:08:18 -0500

    added animations to home screen

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Spring } from "react-spring";
 import { Link } from "react-router-dom";
 import "./Home.css";
-import { Row, Col } from 'react-flexbox-grid';
-import Navbar from './../../Navbar/Navbar';
+import { Row, Col } from "react-flexbox-grid";
+import Navbar from "./../../Navbar/Navbar";
 
 class Home extends Component {
   render() {
@@ -11,14 +12,24 @@ class Home extends Component {
         {/* Top Shelf */}
         <Row className="top-shelf" center="xs" top="xs">
           <Col xs={12}>
-            <Navbar/>
+            <Navbar />
 
             <div className="tech-link-container">
               <Link to="/tech">
-                <button id="tech-link" className="home-link">TECH</button>
+                <Spring
+                  from={{ opacity: 0 }}
+                  to={{ opacity: 1 }}
+                  delay={1000}
+                  config={{ duration: 800 }}
+                >
+                  {props => (
+                    <button style={props} id="tech-link" className="home-link">
+                      TECH
+                    </button>
+                  )}
+                </Spring>
               </Link>
             </div>
-
           </Col>
         </Row>
 
@@ -27,22 +38,56 @@ class Home extends Component {
           <Col xs={0} sm={1} md={1} lg={1}>
             <Row middle="xs">
               <Link to="/about">
-                <button id="about-link" className="home-link">ABOUT</button>
+                <Spring
+                  from={{ opacity: 0 }}
+                  to={{ opacity: 1 }}
+                  delay={1000}
+                  config={{ duration: 800 }}
+                >
+                  {props => (
+                    <button style={props} id="about-link" className="home-link">
+                      ABOUT
+                    </button>
+                  )}
+                </Spring>
               </Link>
             </Row>
           </Col>
 
           <Col xs={12} sm={10} md={10} lg={10}>
-            <div className="logo-box">
-              <h1 className="primary-title">Michael H. Davis</h1>
-              <h2 className="secondary-title">Fullstack Developer</h2>
-            </div>
+            <Spring
+              from={{ opacity: 0 }}
+              to={{ opacity: 1 }}
+              config={{ duration: 800 }}
+            >
+              {props => (
+                <div style={props} className="logo-box">
+                  <h1 className="primary-title">Michael H. Davis</h1>
+                  <h2 className="secondary-title">Fullstack Developer</h2>
+                </div>
+              )}
+            </Spring>
           </Col>
 
           <Col xs={0} sm={1} md={1} lg={1}>
             <Row middle="xs" end="xs">
               <Link to="/portfolio">
-                <button id="portfolio-link" className="home-link">PORTFOLIO</button>
+                <Spring
+                  from={{ opacity: 0 }}
+                  to={{ opacity: 1 }}
+                  delay={1000}
+                  config={{ duration: 800 }}
+                >
+                  {props => (
+                    <button
+                      style={props}
+                      id="portfolio-link"
+                      className="home-link"
+                    >
+                      PORTFOLIO
+                    </button>
+                  )}
+                </Spring>
               </Link>
             </Row>
           </Col>
@@ -51,13 +96,22 @@ class Home extends Component {
         {/* Bottom Shelf */}
         <Row className="bottom-shelf" center="xs" bottom="xs">
           <Col xs={12}>
-
             <div className="contact-link-container">
               <Link to="/contact">
-                <button id="contact-link" className="home-link">CONTACT</button>
+                <Spring
+                  from={{ opacity: 0 }}
+                  to={{ opacity: 1 }}
+                  delay={1000}
+                  config={{ duration: 800 }}
+                >
+                  {props => 
+                    <button style={props} id="contact-link" className="home-link">
+                      CONTACT
+                    </button>
+                  }
+                </Spring>
               </Link>
             </div>
-
           </Col>
         </Row>
       </div>
